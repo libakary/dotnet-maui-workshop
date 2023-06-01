b0a5dfaf89b218b602ec52c5ae6ac3d02cc37e39
Karol <libakary@users.noreply.github.com>
2023-06-01 16:15:54 +0300

    Update BaseViewModel.cs

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,5 +1,56 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
-public class BaseViewModel
+namespace MonkeyFinder.ViewModel;
+
+//[INotifyPropertyChanged]
+public partial class BaseViewModel : ObservableObject
+						   //: INotifyPropertyChanged
 {
+	public BaseViewModel()
+	{
+		//SetProperty(ref isBusy, true);
+	}
+
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(IsNotBusy))]
+	bool isBusy;
+
+	[ObservableProperty]
+	string title;
+
+	public bool IsNotBusy => !IsBusy;
+
+	//public bool IsBusy
+	//{
+	//	get => isBusy;
+	//	set
+	//	{
+	//		if (isBusy == value)
+	//			return;
+
+	//		isBusy = value;
+	//		OnPropertyChanged();
+	//		OnPropertyChanged(nameof(IsNotBusy));
+	//	}
+	//}
+
+	//public  string Title
+	//{
+	//	get => title;
+	//	set
+	//	{
+	//		if (title == value) 
+	//			return;
+
+	//		title = value;
+	//		OnPropertyChanged();
+	//	}
+	//}
+
+	//public event PropertyChangedEventHandler PropertyChanged;
+
+	//public void OnPropertyChanged([CallerMemberName]string name = null)
+	//{
+	//	PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+	//}
 }
